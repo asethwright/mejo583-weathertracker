fa85400f048e4c11be1edfd8c0f1b670006f4dec
Seth Wright <asethwright@gmail.com>
2016-02-11 10:17:25 -0500

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('weatherApp')
+  .controller('MainCtrl', ['$http', function ($http) {
+
+      var _this = this;
+      this.timeslots = [];
+
+      $http.get('/data.json')
+        .success(function(data) {
+            console.log(data);
+            _this.timeslots = _this.filter(data);
+        })
+        .error(function(msg) {
+            console.error("ERROR:" + msg);
+        });
+
+    this.filter = function(data) {
+        for(var i in data) {
+            data[i].date = new Date(
+                data[i].year,
+                (data[i].month - 1),
+                data[i].day,
+                data[i].hour,
+                data[i].minute,
+                data[i].second
+            );
+
+            data[i].nice_date = data[i].month + "/" + data[i].day + "/" + data[i].year + " " + data[i].hour + ":" + data[i].minute;
+        }
+
+        return data;
+    }
+
+  }]);
